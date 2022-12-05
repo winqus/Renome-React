d9c859f88312504878cd35ee6f625f2ebb8776b4
winqus <67459222+winqus@users.noreply.github.com>
2022-12-05 09:12:26 +0200

    Clean up, configurations, additional packages added.

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/jsx-filename-extension': 0,
+    'react/jsx-one-expression-per-line': 0,
+    'react/react-in-jsx-scope': 0,
+    'linebreak-style': 0,
+  },
+};
