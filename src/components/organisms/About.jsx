80d727beffcbce32d20e5ef08f51edf306bde496
winqus <67459222+winqus@users.noreply.github.com>
2022-12-08 17:09:27 +0200

    Implemented about section in React.

---
+++ src/components/organisms/About.jsx
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+
+function About({ title, subtitle, children }) {
+  return (
+    <section className="about">
+      <div className="about__image-container">
+        <img src={`${process.env.PUBLIC_URL}/img/about_img_1.png`} alt="Several white houses" className="about__image about__image--right" loading="lazy" />
+        <img src={`${process.env.PUBLIC_URL}/img/about_img_2.png`} alt="Building seen through tree leaves" className="about__image about__image--left about__image--on-top" loading="lazy" />
+      </div>
+      <h1 className="about__title">{title}</h1>
+      <h2 className="about__title about__subtitle">{subtitle}</h2>
+      <p className="about__text">
+        {children}
+      </p>
+    </section>
+  );
+}
+
+About.propTypes = {
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+export default About;
