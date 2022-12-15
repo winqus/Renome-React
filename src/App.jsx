7b144f6b105fa7fd8f24aeb7c3696a3074ce0660
winqus <67459222+winqus@users.noreply.github.com>
2022-12-15 10:55:17 +0200

    Refactored Gallery component to use data from pageData.json.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Navbar from './components/organisms/Navbar';
 import Carousel from './components/organisms/Carousel';
 import About from './components/organisms/About';
 import Gallery from './components/organisms/Gallery';
-import GalleryImage from './components/atoms/GalleryImage';
 import data from './pageData.json';
 
 function App() {
@@ -11,16 +10,16 @@ function App() {
     <>
       <Navbar title="Renome" />
       <Carousel />
-      <About title={data.about.title} subtitle={data.about.subtitle} text={data.about.text} />
-
-      <Gallery title="gallery" subtitle="MORE ABOUT US VIA PHOTOS">
-        <GalleryImage src={`${process.env.PUBLIC_URL}/img/gallery_img_1.png`} alt="Caffe bar" />
-        <GalleryImage src={`${process.env.PUBLIC_URL}/img/gallery_img_3.png`} alt="Tables inside caffe bar" />
-        <GalleryImage src={`${process.env.PUBLIC_URL}/img/gallery_img_2.png`} alt="Old building basking in sunlight" />
-        <GalleryImage src={`${process.env.PUBLIC_URL}/img/gallery_img_6.png`} alt="Buttermilk donuts" />
-        <GalleryImage src={`${process.env.PUBLIC_URL}/img/gallery_img_5.png`} alt="Salad in a bowl" />
-        <GalleryImage src={`${process.env.PUBLIC_URL}/img/gallery_img_4.png`} alt="Todays specials menu" />
-      </Gallery>
+      <About
+        title={data.about.title}
+        subtitle={data.about.subtitle}
+        text={data.about.text}
+      />
+      <Gallery
+        title={data.gallery.title}
+        subtitle={data.gallery.subtitle}
+        images={data.gallery.images}
+      />
     </>
   );
 }
--- src/components/organisms/Gallery.jsx
+++ src/components/organisms/Gallery.jsx
@@ -1,13 +1,16 @@
+/* eslint-disable react/no-array-index-key */
 import PropTypes from 'prop-types';
 import GalleryImage from '../atoms/GalleryImage';
 
-function Gallery({ title, subtitle, children }) {
+function Gallery({ title, subtitle, images }) {
   return (
     <section className="gallery">
       <h1 className="gallery__title">{title}</h1>
       <h2 className="gallery__subtitle">{subtitle}</h2>
       <div className="gallery__image-container">
-        {children}
+        {images.map((image, index) => (
+          <GalleryImage key={index} src={image.path} alt={image.alt} />
+        ))}
       </div>
     </section>
   );
@@ -16,8 +19,9 @@ function Gallery({ title, subtitle, children }) {
 Gallery.propTypes = {
   title: PropTypes.string.isRequired,
   subtitle: PropTypes.string.isRequired,
-  children: PropTypes.arrayOf(PropTypes.shape({
-    type: PropTypes.oneOf([GalleryImage]),
+  images: PropTypes.arrayOf(PropTypes.shape({
+    path: PropTypes.string.isRequired,
+    alt: PropTypes.string.isRequired,
   })).isRequired,
 };
 
